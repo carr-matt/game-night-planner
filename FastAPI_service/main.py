d8ccd2710a08ab5cff032d7ba3c56f3fa96952c7
Matt <carr.matthew.lee@gmail.com>
2022-11-21 12:20:31 -0600

    random game and name search GET requests working

--- FastAPI_service/main.py
+++ FastAPI_service/main.py
@@ -1,10 +1,16 @@
-import os
+import os, json, requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers import auth
 from routers import accounts
 from routers import sockets
+
+# from routers import account_data
+from routers import boardgame
+
+BGA_ID = os.environ["BGA_ID"]
+
 
 app = FastAPI()
 
@@ -24,3 +30,42 @@ app.add_middleware(
 app.include_router(auth.authenticator.router)
 app.include_router(accounts.router)
 app.include_router(sockets.router)
+app.include_router(boardgame.router)
+
+
+def get_random_game():
+    params = {
+        "random": "true",
+        "pretty": "true",
+        "client_id": BGA_ID,
+    }
+    url = "https://api.boardgameatlas.com/api/search"
+    response = requests.get(url, params=params)
+    content = json.loads(response.content)
+    return content
+
+
+def get_games_by_name(name):
+    params = {
+        "name": name,
+        "limit": 10,
+        "pretty": "true",
+        "fuzzy_match": "true",
+        "client_id": BGA_ID,
+    }
+    url = "https://api.boardgameatlas.com/api/search"
+    response = requests.get(url, params=params)
+    content = json.loads(response.content)
+    return content
+
+
+@app.get("/api/random_game")
+def random_game():
+    data = get_random_game()
+    return data
+
+
+@app.get("/api/games_by_name")
+def games_by_name():
+    data = get_games_by_name()
+    return data

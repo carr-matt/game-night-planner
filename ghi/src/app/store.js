2bc3666db461877c5198de7267fe1d5f9897f534
Joe Monaghan <joemonaghan6@gmail.com>
2022-12-02 12:51:22 -0600

    added detail api

--- ghi/src/app/store.js
+++ ghi/src/app/store.js
@@ -5,6 +5,7 @@ import { authApiSlice } from './authApi';
 import { accountSlice } from './accountSlice';
 import { gameSlice } from './gameApi';
 import { randomSlice } from './randomApi';
+import { detailSlice } from './detailApi';
 
 export const store = configureStore({
   reducer: {
@@ -13,13 +14,15 @@ export const store = configureStore({
     [accountSlice.name]: accountSlice.reducer,
     [gameSlice.reducerPath]: gameSlice.reducer,
     [randomSlice.reducerPath]: randomSlice.reducer,
+    [detailSlice.reducerPath]: detailSlice.reducer,
   },
   middleware: getDefaultMiddleware => {
     return getDefaultMiddleware()
       .concat(apiSlice.middleware)
       .concat(authApiSlice.middleware)
       .concat(gameSlice.middleware)
-      .concat(randomSlice.middleware);
+      .concat(randomSlice.middleware)
+      .concat(detailSlice.middleware);
   },
 });
 

c304d0d3b22a212ff5156e116e50ee704f418908
Margaret Ketchum <mketchum5@gmail.com>
2022-12-02 12:56:47 -0500

    Not working carousel on main page

--- ghi/src/MainDetailComponents/MainPage.js
+++ ghi/src/MainDetailComponents/MainPage.js
@@ -51,28 +51,43 @@ function MainPage(props) {
             </table> */}
 
         </Container>
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://cf.geekdo-images.com/9nGoBZ0MRbi6rdH47sj2Qg__opengraph/img/ol4aoxymd2wCRep9sMujnrx9n_s=/fit-in/1200x630/filters:strip_icc()/pic5786795.jpg" className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block">
+        <h5 className="slide-label">Monopoly</h5>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://www.boardgamequest.com/wp-content/uploads/2013/04/Settlers-of-Catan.jpg" className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block">
+        <h5 className="slide-label">Settlers of Catan</h5>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://cf.geekdo-images.com/c4S2XDRb_DCYCAV-ZAzDpg__opengraph/img/J3e9QE8R-Jc9-Njtj0Mpoh4oT4I=/fit-in/1200x630/filters:strip_icc()/pic288405.jpg" className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block">
+        <h5 className="slide-label">Life</h5>
+      </div>
     </div>
   </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
   );
 }
 

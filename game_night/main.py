ff254963af8b2c0333d854101a70c3af4a020682
Matt <carr.matthew.lee@gmail.com>
2022-12-04 13:38:04 -0600

    popular games endpoint added, swagger docs updates, README updates

--- game_night/main.py
+++ game_night/main.py
@@ -11,7 +11,34 @@ from routers import boardgame
 from routers import owned
 from routers import favorites
 
-app = FastAPI()
+description = """
+This is the API for 🎲 **Game Night Planner** 🎲
+
+a web app for planning game nights with friends.
+
+"""
+
+tags_metadata = [
+    {
+        "name": "Accounts",
+        "description": "Create and manage user accounts.",
+    },
+    {
+        "name": "Board Game Atlas",
+        "description": "Search for board games.",
+        "externalDocs": {
+            "description": "Requests to the Board Game Atlas API.",
+            "url": "https://www.boardgameatlas.com/api/docs",
+        },
+    },
+]
+
+app = FastAPI(
+    title="Game Night Planner",
+    description=description,
+    version="0.0.1",
+    openapi_tags=tags_metadata,
+)
 
 origins = [
     "https://localhost:3000",
@@ -28,9 +55,15 @@ app.add_middleware(
 )
 
 
-app.include_router(auth.authenticator.router)
+app.include_router(
+    auth.authenticator.router,
+    tags=["Accounts"],
+)
 app.include_router(accounts.router)
 app.include_router(sockets.router)
-app.include_router(boardgame.router)
+app.include_router(
+    boardgame.router,
+    tags=["Board Game Atlas"],
+)
 app.include_router(owned.router)
 app.include_router(favorites.router)

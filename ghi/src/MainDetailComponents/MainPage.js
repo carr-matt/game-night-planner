d093a2a9b877ad6c34508e7f47d139c6ccd6903f
Margaret Ketchum <mketchum5@gmail.com>
2022-12-05 11:50:09 -0500

    finally got the image to show up on random game

--- ghi/src/MainDetailComponents/MainPage.js
+++ ghi/src/MainDetailComponents/MainPage.js
@@ -30,24 +30,20 @@ function MainPage() {
               <div className="card-header">
                 <h2>Random Game</h2>
               </div>
-              <div className="card-body">
-                <table>
-                  <tbody>
+                <div className="card">
+
                     {data.games?.map(randomGame => (
-                      <tr key={randomGame.id}>
-                        <td>
-                          <a href={randomGame.image_url} />
-                        </td>
-                        <td>
-                          {randomGame.name}
-                        </td>
-                        <td>
-                          {randomGame.description}
-                        </td>
-                      </tr>
+                      <div key={randomGame.id}>
+
+                        <img src={randomGame.image_url} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                         <h3 className="card-title"> {randomGame.name} </h3>
+
+                          <p className="card-text"> {randomGame.description} </p>
+                        </div>
+                      </div>
                     ))}
-                  </tbody>
-                </table>
+                 </div>
               </div>
             </div>
           </div>
@@ -55,43 +51,7 @@ function MainPage() {
         </div>
 
 
-        {/* <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="https://cf.geekdo-images.com/9nGoBZ0MRbi6rdH47sj2Qg__opengraph/img/ol4aoxymd2wCRep9sMujnrx9n_s=/fit-in/1200x630/filters:strip_icc()/pic5786795.jpg" className="d-block w-100" alt="..." />
-      <div className="carousel-caption d-none d-md-block">
-        <h5 className="slide-label">Monopoly</h5>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="https://www.boardgamequest.com/wp-content/uploads/2013/04/Settlers-of-Catan.jpg" className="d-block w-100" alt="..." />
-      <div className="carousel-caption d-none d-md-block">
-        <h5 className="slide-label">Settlers of Catan</h5>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="https://cf.geekdo-images.com/c4S2XDRb_DCYCAV-ZAzDpg__opengraph/img/J3e9QE8R-Jc9-Njtj0Mpoh4oT4I=/fit-in/1200x630/filters:strip_icc()/pic288405.jpg" className="d-block w-100" alt="..." />
-      <div className="carousel-caption d-none d-md-block">
-        <h5 className="slide-label">Life</h5>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button> */}
-{/* </div> */}
-</div>
-  );
-}
+
+)}
 
 export default MainPage;

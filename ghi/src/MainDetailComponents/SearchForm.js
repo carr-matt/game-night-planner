41311227c8e60dcd696c7f691d8f40dbec50c868
Margaret Ketchum <mketchum5@gmail.com>
2022-12-06 16:44:11 -0500

    not working search form dropdowns

--- ghi/src/MainDetailComponents/SearchForm.js
+++ ghi/src/MainDetailComponents/SearchForm.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import './compy.css';
+import { useGetGamesQuery } from '../app/api';
+import { useGetMechanicQuery } from '../app/mechanics'
+
+// hard code min players, min time and max time and make it so they search through moneymaker
+// for something greater than or equal to or less than or equal to
+
+
+
 
 function SearchForm() {
+
+
+    const { data: gameData , isLoading: isGameLoading } = useGetGamesQuery();
+    const {data: mechanicData , isLoading: isMechanicLoading } = useGetMechanicQuery();
+    console.log(gameData, mechanicData)
+
+
+   if (isGameLoading) {
+    return null}
+
+  if (isMechanicLoading) {
+    return null}
+
+
     return (
 
               <div className="row" >
@@ -10,8 +32,8 @@ function SearchForm() {
                 <h1 className="header-title">Search Form</h1>
                 <form id="search-form">
                  <div className="mb-3">
-                    <select required name="age-range" type="text" className="form-select">
-                      <option value="">Age Range</option>
+                    <select required name="minimum-age" type="text" className="form-select">
+                      <option value="">Minimum Age</option>
                     </select>
                   </div>
                  <div className="mb-3">
@@ -27,16 +49,35 @@ function SearchForm() {
                  <div className="mb-3">
                     <select required name="play-time" type="text" className="form-select">
                       <option value="">Play Time</option>
+                      {gameData.games?.map(playtime => {
+                        return (
+                            <option key={`${playtime.min_playtime}, ${playtime.id}`} value={playtime.id}>{playtime.min_playtime}
+                            </option>
+                        )
+                      })}
                     </select>
                   </div>
                  <div className="mb-3">
-                    <select required name="ratings" type="text" className="form-select">
-                      <option value="">Ratings</option>
+                    <select required name="mechanics" type="text" className="form-select">
+                      <option value="">Mechanics</option>
+                      {gameData.games.mechanics?.map(mechanic => {
+                        return (
+                            <option key={`${gameData.mechanic}`} value={gameData.mechanic}>
+                            </option>
+                        )
+                      })}
                     </select>
                   </div>
                   <div className="mb-3">
-                    <select required name="genre" type="text" className="form-select">
-                      <option value="">Genre</option>
+                    <select required name="category" type="text" className="form-select">
+                      <option value="">Category</option>
+                        {gameData.games.categories?.map(category => {
+                        return (
+                            <option key={category} value={category}>
+                            </option>
+                        )
+                      })}
+
                     </select>
                   </div>
                   <button className="btn btn-secondary">Search</button>

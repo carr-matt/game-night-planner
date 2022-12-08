af7d3903549ba26c22370a505cc901a48f402995
Jazz <jazz.stewart@outlook.com>
2022-12-08 16:41:31 -0500

    please no more styling

--- ghi/src/Login.js
+++ ghi/src/Login.js
@@ -31,44 +31,87 @@ useEffect(() => {
   handleLogin();
 }, [loginSuccessful]);
 
-
-  return (
-        <div className="box content" style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "80vh"
-        }}>
-          <h3>Log In</h3>
-          { error ? <Alert variant="danger">{error.data.detail}</Alert> : null }
-      <form method="POST" onSubmit={ async (e) => {
+return (
+    <div className="Auth-form-container">
+      <form className="Auth-form bg-dark centered-form" method='POST' onSubmit={ async (e) => {
         e.preventDefault(); await logIn(e.target); handleLogin();}}>
-            <div className="field">
-              <label className="label" htmlFor="email">Email</label>
-              <div className="control">
-                <input required onChange={field} value={username} name="username" className="input" type="username" placeholder="example@example.com" />
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Password</label>
-              <div className="control">
-                <input required onChange={field} value={password} name="password" className="input" type="password" placeholder="password" />
-              </div>
-            </div>
-            <div className="field is-grouped">
-              <div className="control">
-                <button disabled={logInLoading} className="button is-primary">Submit</button>
-              </div>
-              <div className="control">
-                <button
-                  type="button"
-                  onClick={() => dispatch((null))}
-                  className="button">Cancel</button>
-              </div>
-            </div>
-          </form>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          { error ? <Alert variant="danger">{error.data.detail}</Alert> : null }
+                    <div className="text-center">
+            <a href="/SignUp">Need to Register?{" "}</a>
+          </div>
+          <div className="form-group mt-3">
+            <label htmlFor='email'>Email address</label>
+            <input
+              required onChange={field} value={username} name="username"
+              type="username"
+              className="form-control mt-1"
+              placeholder="example@example.com"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              required onChange={field} value={password} name="password"
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button disabled={logInLoading} type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot your password? Too bad. {'>'}:)
+          </p>
         </div>
-  );
+      </form>
+    </div>
+  )
 }
+
+
+
+  // return (
+  //       <div className="box content" style={{
+  //         display: "flex",
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //         height: "80vh"
+  //       }}>
+  //         <h3>Log In</h3>
+  //         { error ? <Alert variant="danger">{error.data.detail}</Alert> : null }
+  //     <form method="POST" onSubmit={ async (e) => {
+  //       e.preventDefault(); await logIn(e.target); handleLogin();}}>
+  //           <div className="field">
+  //             <label className="label" htmlFor="email">Email</label>
+  //             <div className="control">
+  //               <input required onChange={field} value={username} name="username" className="input" type="username" placeholder="example@example.com" />
+  //             </div>
+  //           </div>
+  //           <div className="field">
+  //             <label className="label">Password</label>
+  //             <div className="control">
+  //               <input required onChange={field} value={password} name="password" className="input" type="password" placeholder="password" />
+  //             </div>
+  //           </div>
+  //           <div className="field is-grouped">
+  //             <div className="control">
+  //               <button disabled={logInLoading} className="button is-primary">Submit</button>
+  //             </div>
+  //             <div className="control">
+  //               <button
+  //                 type="button"
+  //                 onClick={() => dispatch((null))}
+  //                 className="button">Cancel</button>
+  //             </div>
+  //           </div>
+  //         </form>
+  //       </div>
+  // );
+// }
 
 export default Login;

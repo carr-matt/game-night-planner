f665248e145285a8fdd4bd4b705174d3ee0c7f8b
Joe Monaghan <joemonaghan6@gmail.com>
2022-12-08 15:13:16 -0600

    IT WORKS OMG

--- ghi/src/DashBoardComponents/UserDashboard.js
+++ ghi/src/DashBoardComponents/UserDashboard.js
@@ -4,7 +4,9 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import './comp.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { gameSlice } from "../app/gameApi";
+
 
   const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,62 +15,83 @@ import { gameSlice } from "../app/gameApi";
   textAlign: 'center',
   color: theme.palette.text.secondary,
   }));
-// import Item from '@material-ui/core/Item';
+
 
 function UserDashboard(props) {
     const {useGetOwnedQuery, useGetFavoriteQuery} = gameSlice //use_query sandwhich lets redux know that this is a hook
     const [collectionOfGames, setCollectionOfGames] = useState({}) //setcollection is a function which recieves the selected data from our handle change on collection of games
     const [collectionOfFavGames, setCollectionOfFavGames] = useState({})
-    const {data, isLoading} = useGetOwnedQuery();
-    const {favData, isFavLoading} = useGetFavoriteQuery();
-    const [ownedGame, setOwnedGame ] = useState(null);
-    const [favoriteGame, setFavoriteGame ] = useState(null);
+    const {data, error: gameError, isLoading} = useGetOwnedQuery(); 
+    const {data: favData, error: favGameError, isLoading: isFavLoading} = useGetFavoriteQuery();
+    const [ownedGame, setOwnedGame ] = useState(null); 
+    const [favoriteGame, setFavoriteGame ] = useState(null); 
 
     //react router on change to link
     useEffect(() => console.log(ownedGame), [ownedGame])
-    useEffect(() => console.log(favoriteGame), [favoriteGame])
+    useEffect(() =>  console.log(favoriteGame), [favoriteGame])
 
     useEffect(() =>{
-
+      
       if(data){
         const map = {}
-        data.owned_list.forEach(game =>{
+        data.owned_list.forEach(game =>{ 
           map[game.bgaID] = game
         }) //go through the game data and (forEach) means to create a bucket
         // for(let i = 0; i < data.games.length; i++){//same thing as above
         //   const game = data.games[i]
         //   collectionOfGames[game.id] = game
-        // } == what forEach does.
+        // } == what forEach does. 
         setCollectionOfGames(map)// we are taking this temporay bucket of games (map) and setting it equal to our variable collection of games to persitst the data over reRenders
       }
       if(favData){
         const mapp = {}
-        favData.favorites.forEach(favGame =>{
+        favData.favorites.forEach(favGame =>{ 
           mapp[favGame.bgaID] = favGame
            })
           setCollectionOfFavGames(mapp)
-      }
+          console.log(favData)
+      } 
     },[data, favData])
+
+    
+    const handleClick = (e) => {
+      e.preventDefault();
+      console.log('The link was clicked.');
+  }
 
     const handleChange = e =>{
       console.log('event',e,  e.target.value, 'collectionOfGames', collectionOfGames)
+
+      // window.location.href = `${window.location.origin}/detail/` //$ equls string interpalation only with backticks
+      window.location.href = `${window.location.origin}/detail/${e.target.value}`
+
       const selectedGame = collectionOfGames[e.target.value]
       setOwnedGame(selectedGame)
+      
 
       console.log('event',e,  e.target.value, 'collectionOfFavGames', collectionOfFavGames)
       const selectedfavGame = collectionOfFavGames[e.target.value]
       setFavoriteGame(selectedfavGame)
     }
 
-    if(isLoading) {
-      return null
-    }
+    // if(isLoading || gameError) {
+    //   return (<React.Fragment>Loading...</React.Fragment>)
+    // }
+      if(isLoading) {
+        return null
+      }
 
-     if(isFavLoading) {
-      return null
-    }
+      if(isFavLoading) {
+        return null
+      }
 
-    return (
+    
+
+    //  if(isFavLoading  || favGameError) {
+    //   return (<React.Fragment>Loading...</React.Fragment>)
+    // }
+    
+    return (  
     <div className="app-list">
       <h1 className="Header">User Dashboard </h1>
         <Box sx={{ flexGrow: 1 }}>
@@ -77,25 +100,28 @@ function UserDashboard(props) {
             <Item> Owned Games </Item> {/* xs = the height and md = the column space it takes up out of 12 */}
             <Item> <select id="owned-form1" className="form-control" onChange={handleChange}>  {/* {ownedGames.map(ownedGame => { */}
                     <option id="owned-form1" value="">Your Owned Games</option>
-                    {data.owned_list.map( game => {
+                    {data.owned_list?.map( game => {
                     return (
-                      <option key={`${game.name} ${game.bgaID}`} value={game.bgaID}>{game.name} </option>
+                      <option key={`${game.name} ${game.bgaID}`} value={game.bgaID}>{game.name} </option> 
+                      
                     )
-                  })}  </select> </Item>
+                  })}  </select> </Item> 
           </Grid>
           <Grid item xs={6} md={5}>
-            <Item> Favorite Games </Item>
-            <Item> <select id="owned-form1" className="form-control" onChange={handleChange}>
+            <Item> Favorite Games </Item> 
+            <Item> <select id="owned-form1" className="form-control" onChange={handleChange}>  
                     <option id="owned-form1" value="">Your Favorite Games</option>
                     {favData?.favorites.map( favGame => {
                     return (
-                      <option key={`${favGame.name} ${favGame.bgaID}`} value={favGame.bgaID}>{favGame.name}</option>
+                      <option key={`${favGame.name} ${favGame.bgaID}`} value={favGame.bgaID}>{favGame.name} </option>
+                      
                     )
-                  })}  </select> </Item>
+                  })}  </select> 
+                  </Item> 
           </Grid>
         </Grid>
         </Box>
-         </div>
+         </div> 
 
     )
 
---
+++ ghi/src/MainDetailComponents/DetailPage.js
@@ -0,0 +1,213 @@
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from 'react-router-dom'
+import { useGetDetailQuery } from '../app/detailApi';
+import { useParams } from "react-router-dom";
+import { useAddNewPostMutation } from "../app/createSlice"; 
+
+
+
+
+function DetailPage() {
+
+   let { bgaID } = useParams();
+   const { data, isLoading } = useGetDetailQuery(bgaID)
+  //  console.log(data)
+  //  console.log(useParams)
+      console.log(bgaID)
+      console.log(data)
+// const { data: postData } = api.endpoints.getPosts.useQuery()
+// const [updatePost, { data:updatePostData }] = api.endpoints.updatePost.useMutation()
+const [addNewGame, response] = useAddNewPostMutation()
+// const { data: updatePostData } = useAddNewPostMutation();
+
+
+
+
+
+   if (isLoading) {
+    return null
+  }
+    //  const handleOwnedClick = (e) => {
+    //     addNewGame(bgaID.bgaId, bgaID.name)
+    //     }
+  
+      // const handleOwnedClick = e => {
+      // data.games( detailGame => {
+      //   map[detailGame.id, detailGame.name] = detailGame
+      // addNewGame(detailGame) }
+
+     const handleOwnedClick = e => {
+      {data.games?.map( detailGame => {
+        const idName = {"bgaID": detailGame.id,  "name": detailGame.name}
+        addNewGame(idName) }) 
+         } } 
+        //  detailGame.id,
+      // e.preventDefault();
+      // console.log(detailGame)
+      console.log('The link was clicked.');
+      
+            
+
+
+
+  return (
+    <div>
+        <h1>Game Night Planner</h1>
+        <h2>Never wonder what game you'll play next again!</h2>
+        <br/>
+        <div className="container">
+        <div className="row mt-5">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">
+                {data.games?.map(detailGame => (
+                <h3 key={detailGame.games}>
+                   {detailGame.name}</h3>)
+                ) }
+
+                <button onClick={handleOwnedClick}> Add to Owned List</button>
+                
+              </div>
+
+              <div className="card-body">
+                <table>
+                  <tbody>
+                    {data.games?.map(detailGame => (
+                      <tr key={detailGame.games}>
+                        <td>
+                          <tr> <img src={detailGame.image_url} /> </tr>
+                        </td>
+                        <td>
+                          <tr> {detailGame.name} </tr>
+                        </td>
+                        <td>
+                          {detailGame.description}
+                        </td>
+                        <td>
+                          Minimum players: {detailGame.min_players}
+                        </td>
+                        <td>
+                          Maximum players: {detailGame.max_players}
+                        </td>
+                        <td>
+                          Minimum age: {detailGame.min_age}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+)}
+
+export default DetailPage
+
+
+
+
+
+
+//     async function addOwned(token, bgaID, name) {
+//     const ownedUrl = "http:localhost:8000/owned" 
+//     const fetchConfig = {
+//         method: 'POST',
+//         body: JSON.stringify({ "bgaID": bgaID, "name": name }),
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//             credentials: "include",
+//             accept: "application/json",
+//             "Content-Type": "application/json"
+//         }
+
+//     };
+//     // await fetch(ownedUrl, fetchConfig);
+// }
+
+//   async function addFavorite(token, bgaID, name) {
+//       const favUrl = `${process.env.REACT_APP_API_HOST}/favorite`
+//       const fetchConfig = {
+//           method: 'POST',
+//           body: JSON.stringify({ "bgaID": bgaID, "name": name }),
+//           headers: {
+//               Authorization: `Bearer ${token}`,
+//               credentials: "include",
+//               accept: "application/json",
+//               "Content-Type": "application/json"
+//           }
+//       };
+//       // await fetch(favUrl, fetchConfig);
+//   }
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { Grid } from '@mui/material';
+// import { styled } from '@mui/material/styles';
+// import Paper from '@mui/material/Paper';
+// import Box from '@mui/material/Box';
+// import 'bootstrap/dist/css/bootstrap.css'
+// import './compy.css';
+// import { detailSlice } from "../app/detailApi";
+// import 'bootstrap/dist/css/bootstrap.css'
+
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+//   }));
+
+
+// function DetailPage (props) {s
+//   const {useGetDetailQuery} = detailSlice
+//   const {data, isLoading} = useGetDetailQuery();
+//   const [collectionOfDetails, setCollectionOfDetails] = useState({})
+
+//     useEffect(() =>{
+//        if(favData){
+//         const map = {}
+//         data.games.forEach(game =>{
+//           map[game.image_url] = game
+//         })
+//         setCollectionOfDetails(map)
+
+//     }
+
+//     const handleChange = e =>{
+//       console.log('event',e,  e.target.value, 'collectionOfDetails', collectionOfDetails)
+//       const selectedGame = collectionOfDetails[e.target.value]
+//       setDetailGame(selectedGame)
+
+//     if(isLoading) {
+//       return null
+//     }
+
+//   return (
+//     <Box sx={{ flexGrow: 1 }}>
+//          <Grid container spacing={5}> {/* space between Items in grid */}
+//           <Grid item xs={6} md={5}>
+//             <Item> Game Details </Item>
+
+
+
+
+//         </Grid>
+//         </Grid>
+//         </Box>
+//   )
+
+// }
+
+
+// export default DetailPage

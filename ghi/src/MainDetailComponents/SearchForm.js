f23f8bcb6775c767c989ed8078faa93ec93c5375
Margaret Ketchum <mketchum5@gmail.com>
2022-12-08 10:54:33 -0500

    cleaned up some code

--- ghi/src/MainDetailComponents/SearchForm.js
+++ ghi/src/MainDetailComponents/SearchForm.js
@@ -16,35 +16,15 @@ function SearchForm() {
     const {data: mechanicData , isLoading: isMechanicLoading } = useGetMechanicQuery();
     const {data: categroyData , isLoading: isCategoryLoading } = useGetCategoryQuery();
 
-    let min_players = [
-      {label: "1", value: "1"},
-      {label: "2", value: "2"},
-      {label: "3", value: "3"},
-      {label: "4", value: "4"},
-      {label: "5", value: "5"},
-      {label: "6", value: "6"},
-      {label: "7", value: "7"},
-      {label: "8", value: "8"},
-      {label: "9", value: "9"},
-      {label: "10", value: "10"},
+    let min_players = []
+      for (let i = 1; i <= 10; i++) {
+        min_players.push({label: String(i), value: String(i)})
+      }
 
-    ]
-
-    let max_players = [
-      {label: "4", value: "4"},
-      {label: "5", value: "5"},
-      {label: "6", value: "6"},
-      {label: "7", value: "7"},
-      {label: "8", value: "8"},
-      {label: "9", value: "9"},
-      {label: "10", value: "10"},
-      {label: "11", value: "11"},
-      {label: "12", value: "12"},
-      {label: "13", value: "13"},
-      {label: "14", value: "14"},
-      {label: "15", value: "15"},
-
-    ]
+    let max_players = []
+      for (let i = 4; i <= 15; i++) {
+        max_players.push({label: String(i), value: String(i)})
+      }
 
     let play_time = [
       {label: "15 minutes", value: "15"},

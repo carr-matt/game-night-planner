3a7820aa91528b9d3841d954dd8d65d9d631a54e
Joshua Clay <jdclay01@gmail.com>
2022-12-08 11:55:21 -0600

    main page with functional dynamic carousel

--- ghi/src/MainDetailComponents/DetailPage.js
+++ ghi/src/MainDetailComponents/DetailPage.js
@@ -1,71 +1,71 @@
-import React, { useEffect } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import { useGetCarouselQuery } from '../app/carouselApi';
-import "./Carousel.css";
-import Container from "react-bootstrap/esm/Container";
+// import React, { useEffect } from 'react';
+// import Carousel from 'react-bootstrap/Carousel';
+// import { useGetCarouselQuery } from '../app/carouselApi';
+// import "./Carousel.css";
+// import Container from "react-bootstrap/esm/Container";
 
 
-function DetailPage() {
-  const {data: carouselData, isLoading: carouselIsLoading } = useGetCarouselQuery();
-  // console.log(data);
+// function DetailPage() {
+//   const {data: carouselData, isLoading: carouselIsLoading } = useGetCarouselQuery();
+//   // console.log(data);
 
-  if (carouselIsLoading){
-    return null
-  };
+//   if (carouselIsLoading){
+//     return null
+//   };
 
-  return (
-    <Container className="popular-game-carousel">
-      <Carousel style={{width: "100%"}}>
-        {carouselData.games?.map(popularGames => (
-          <Carousel.Item key={popularGames.id}>
-          <div>
-          <h4 className="game-title"> {popularGames.name} </h4>
-            <div style={{display: "flex"}}>
-              <span className='m-2' style={{width: '100%'}}>
-                <img
-                  className="d-block"
-                  src={popularGames.image_url}
-                  alt="carousel-images"
-                  style={{width: '20%'}}
-                  />
-              </span>
-            </div>
-            </div>
-          </Carousel.Item>
-        ))
-        }
-      </Carousel>
-    </Container>
-  );
-      //   {data.games?.map(popularGame => (
-      //     <Carousel.Item key={popularGame["2"].id}>
-      //       <div style={{display: "flex"}}>
-      //         <span className='m-3' style={{width: "100%"}} >
-      //           <img
-      //             className="d-block w-20"
-      //             src={popularGame["2"].image_url}
-      //             alt="Second slide"
-      //             style={{width: "30%"}}
-      //           />
-      //         </span>
-      //       </div>
-      //     </Carousel.Item>
-      //   )
-      //     <Carousel.Item>
-      //       <div style={{display: "flex"}}>
-      //         <span className='m-3' style={{width: "100%"}} >
-      //           <img
-      //             className="d-block w-20"
-      //             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXVl0KDpMwIW-lbkbVnFk6xtqCsWF_d2C4zWb6V7b7qkPJkecxFIdMgpD05iZwEhkH7lc&usqp=CAU"
-      //             alt="Third slide"
-      //             style={{width: "16.7%"}}
-      //           />
-      //         </span>
-      //       </div>
-      //     </Carousel.Item>
-      // )}
+//   return (
+//     <Container className="popular-game-carousel">
+//       <Carousel style={{width: "100%"}}>
+//         {carouselData.games?.map(popularGames => (
+//           <Carousel.Item key={popularGames.id}>
+//           <div>
+//           <h4 className="game-title"> {popularGames.name} </h4>
+//             <div style={{display: "flex"}}>
+//               <span className='m-2' style={{width: '100%'}}>
+//                 <img
+//                   className="d-block"
+//                   src={popularGames.image_url}
+//                   alt="carousel-images"
+//                   style={{width: '20%'}}
+//                   />
+//               </span>
+//             </div>
+//             </div>
+//           </Carousel.Item>
+//         ))
+//         }
 //       </Carousel>
-// </Container>
-// </div>
-      }
-export default DetailPage
+//     </Container>
+//   );
+//       //   {data.games?.map(popularGame => (
+//       //     <Carousel.Item key={popularGame["2"].id}>
+//       //       <div style={{display: "flex"}}>
+//       //         <span className='m-3' style={{width: "100%"}} >
+//       //           <img
+//       //             className="d-block w-20"
+//       //             src={popularGame["2"].image_url}
+//       //             alt="Second slide"
+//       //             style={{width: "30%"}}
+//       //           />
+//       //         </span>
+//       //       </div>
+//       //     </Carousel.Item>
+//       //   )
+//       //     <Carousel.Item>
+//       //       <div style={{display: "flex"}}>
+//       //         <span className='m-3' style={{width: "100%"}} >
+//       //           <img
+//       //             className="d-block w-20"
+//       //             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXVl0KDpMwIW-lbkbVnFk6xtqCsWF_d2C4zWb6V7b7qkPJkecxFIdMgpD05iZwEhkH7lc&usqp=CAU"
+//       //             alt="Third slide"
+//       //             style={{width: "16.7%"}}
+//       //           />
+//       //         </span>
+//       //       </div>
+//       //     </Carousel.Item>
+//       // )}
+// //       </Carousel>
+// // </Container>
+// // </div>
+//       }
+// export default DetailPage
--- ghi/src/MainDetailComponents/MainPage.js
+++ ghi/src/MainDetailComponents/MainPage.js
@@ -1,7 +1,11 @@
 //will need use effect to get the data for the filters
 import { useNavigate, Link } from 'react-router-dom'
 import { useGetRandomQuery } from '../app/randomApi';
-import { useParams } from "react-router-dom";
+import React, { useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { useGetCarouselQuery } from '../app/carouselApi';
+import "./MainPage.css";
+import Container from "react-bootstrap/esm/Container";
 // make the random game image clickable to bring us to the detail page for that game
 
 
@@ -13,10 +17,14 @@ function MainPage() {
   const { data: randomData, isLoading: randomIsLoading} = useGetRandomQuery();
   //  console.log(data)
 
+  const {data: carouselData, isLoading: carouselIsLoading } = useGetCarouselQuery();
 
 
    if (randomIsLoading) {
     return null}
+   if (carouselIsLoading) {
+    return null}
+
 
   const openInNewTab = url => {
     window.open(url, '_blank', 'noopener,noreferrer');
@@ -25,41 +33,63 @@ function MainPage() {
 
 
   return (
-    <div>
+    <React.Fragment>
+    <div className="main-page">
+      <Container className="banner px-4 mb-5 mt-5 text-center rounded">
         <h1>Game Night Planner</h1>
-        <h2>Never wonder what game you'll play next again!</h2>
-        <br/>
-        <div className="container">
-        <div className="row mt-5">
-          <div className="col">
-            <div className="card">
-              <div className="card-header">
-                <h2>Random Game</h2>
-              </div>
-                <div className="image-card">
-
+        <h4>Never wonder what game you'll play next again!</h4>
+      </Container>
+      <div>
+        <Container className="random-game-container">
+            <div className="col">
+              <div className="card">
+                <div className="card-header">
+                  <h2>Click here to Generate a Random Game</h2>
+                </div>
+                  <div className="image-card">
                     {randomData.games?.map(randomGame => (
-                      <div key={randomGame.id}>
+                        <div key={randomGame.id}>
 
-                        <img src={randomGame.image_url} onClick={() => openInNewTab('http://localhost:3000/detail')} className="random-img" alt="..." />
-                        <div className="card-body">
-                         <h3 className="card-title"> {randomGame.name} </h3>
+                          <img src={randomGame.image_url} onClick={() => openInNewTab('http://localhost:3000/detail')} className="random-img" alt="..." />
+                          <div className="card-body">
+                          <h3 className="card-title"> {randomGame.name} </h3>
 
-                          <p className="card-text"> {randomGame.description} </p>
+                            <p className="card-text"> {randomGame.description} </p>
+                          </div>
                         </div>
-                      </div>
-                    ))}
-                 </div>
-              </div>
+                    )
+                    )};
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-
-        </div>
-
-
-
+          </Container>
+      </div>
+      <div>
+        <Container className="popular-game-carousel">
+          <Carousel style={{width: "100%"}}>
+            {carouselData.games?.map(popularGames => (
+              <Carousel.Item key={popularGames.id}>
+                <div>
+                  <h4 className="game-title"> {popularGames.name} </h4>
+                    <div style={{display: "flex"}}>
+                      <span className='m-2' style={{width: '100%'}}>
+                        <img
+                          className="d-block"
+                          src={popularGames.image_url}
+                          alt="carousel-images"
+                          style={{width: '20%'}}
+                        />
+                      </span>
+                    </div>
+                </div>
+            </Carousel.Item>
+            )
+          )};
+          </Carousel>
+        </Container>
+      </div>
+      </div>
+      </React.Fragment>
 );
-}
-
+            }
 export default MainPage;

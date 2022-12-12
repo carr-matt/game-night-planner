339c8c3b78e2d63f7ede788e12f960bbb5bfdcf1
Jazz <jazz.stewart@outlook.com>
2022-12-12 13:20:22 -0500

    styling of the user dashboard

--- ghi/src/DashBoardComponents/UserDashboard.js
+++ ghi/src/DashBoardComponents/UserDashboard.js
@@ -5,10 +5,11 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import './comp.css';
 import { gameSlice } from "../app/gameApi";
+import { deepOrange, orange } from '@mui/material/colors';
 
 
   const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: 'orange',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',

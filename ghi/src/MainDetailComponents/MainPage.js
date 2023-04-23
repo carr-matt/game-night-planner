a53095b88bd508a34d42af0fdc7f1a75dd4adb3a
Matt <carr.matthew.lee@gmail.com>
2023-04-23 09:46:01 -0500

    code formatting

--- ghi/src/MainDetailComponents/MainPage.js
+++ ghi/src/MainDetailComponents/MainPage.js
@@ -1,69 +1,76 @@
 import { useGetRandomQuery } from '../app/randomApi';
 import React from 'react';
-import "./MainPage.css";
-import Container from "react-bootstrap/esm/Container";
-
-
-
+import './MainPage.css';
+import Container from 'react-bootstrap/esm/Container';
 
 function MainPage() {
+  const { data: randomData, isLoading: randomIsLoading } = useGetRandomQuery();
 
-
-
-  const { data: randomData, isLoading: randomIsLoading} = useGetRandomQuery();
-
-
-   if (randomIsLoading) {
-    return null}
-
-
-  const openInNewTab = url => {
-    window.open(url, '_blank', 'noopener,noreferrer');
-
+  if (randomIsLoading) {
+    return null;
   }
+
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
 
   const refreshPage = () => {
     window.location.reload();
-
-  }
-
+  };
 
   return (
     <React.Fragment>
-    <div className="main-page">
-      <Container className="banner px-4 mb-5 mt-5 text-center rounded">
-        <h1>Game Night Planner</h1>
-        <h5>Never wonder what game you'll play next again!</h5>
-      </Container>
-      <div>
-        <Container className="random-game-container">
+      <div className="main-page">
+        <Container className="banner px-4 mb-5 mt-5 text-center rounded">
+          <h1>Game Night Planner</h1>
+          <h5>Never wonder what game you'll play next again!</h5>
+        </Container>
+        <div>
+          <Container className="random-game-container">
             <div className="col">
               <div className="card bg-dark">
                 <div className="card-header">
-                  <h2 onClick={refreshPage} to="/">Generate a Random Game</h2>
+                  <h2
+                    onClick={refreshPage}
+                    to="/"
+                  >
+                    Generate a Random Game
+                  </h2>
                 </div>
-                  <div className="image-card">
-                    {randomData.games?.map(randomGame => (
-                        <div key={randomGame.id}>
-
-                          <img src={randomGame.image_url} onClick={() => openInNewTab(`${process.env.PUBLIC_URL}/detail/${randomGame.id}`)} className="random-img" alt="..." />
-                          <div className="card-body">
-                          <h3 className="card-title"> {randomGame.name} </h3>
-
-                            <p className="card-text"> {randomGame.description} </p>
-                          </div>
-                        </div>
-                    )
-                    )};
+                <div className="image-card">
+                  {randomData.games?.map((randomGame) => (
+                    <div key={randomGame.id}>
+                      <img
+                        src={randomGame.image_url}
+                        onClick={() =>
+                          openInNewTab(
+                            `${process.env.PUBLIC_URL}/detail/${randomGame.id}`
+                          )
+                        }
+                        className="random-img"
+                        alt="..."
+                      />
+                      <div className="card-body">
+                        <h3 className="card-title"> {randomGame.name} </h3>
+                        <p className="card-text">
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: randomGame.description,
+                            }}
+                          />{' '}
+                        </p>
+                      </div>
                     </div>
+                  ))}
+                  ;
                 </div>
+              </div>
             </div>
           </Container>
+        </div>
+        <div></div>
       </div>
-      <div>
-      </div>
-      </div>
-      </React.Fragment>
-);
-            }
+    </React.Fragment>
+  );
+}
 export default MainPage;
--- ghi/src/MainDetailComponents/Trending.js
+++ ghi/src/MainDetailComponents/Trending.js
@@ -1,55 +1,63 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useGetCarouselQuery } from '../app/carouselApi';
-import "./MainPage.css";
-import Container from "react-bootstrap/esm/Container";
-
-
-
+import './MainPage.css';
+import Container from 'react-bootstrap/esm/Container';
 
 function Trending() {
+  const { data: carouselData, isLoading: carouselIsLoading } =
+    useGetCarouselQuery();
 
-
-  const { data: carouselData, isLoading: carouselIsLoading } = useGetCarouselQuery();
-
-   if (carouselIsLoading) {
-    return null}
-
-
-  const openInNewTab = url => {
-    window.open(url, '_blank', 'noopener,noreferrer');
-
+  if (carouselIsLoading) {
+    return null;
   }
 
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
 
   return (
     <React.Fragment>
-        <Container className="popular-game-carousel">
-          <div className='card bg-dark'>
-            <h1>Trending Games of the Week</h1>
-          <Carousel style={{width: "100%"}}>
-            {carouselData.games?.map(popularGames => (
+      <Container className="popular-game-carousel">
+        <div className="card bg-dark">
+          <h1>Trending Games of the Week</h1>
+          <Carousel style={{ width: '100%' }}>
+            {carouselData.games?.map((popularGames) => (
               <Carousel.Item key={popularGames.id}>
                 <div>
                   <h4 className="game-title"> {popularGames.name} </h4>
-                    <div style={{display: "flex"}}>
-                      <span className='m-2' style={{width: '100%'}}>
-                        <img
-                          className="d-block"
-                          src={popularGames.image_url} onClick={() => openInNewTab(`${process.env.PUBLIC_URL}/detail/${popularGames.id}`)}
-                          alt="carousel-images"
-                          style={{width: '20%'}}
-                        />
-                      </span>
-                    </div>
+                  <div
+                  // style={{display: "flex"}}
+                  >
+                    <span
+                      className="m-2"
+                      style={{ width: '100%' }}
+                    >
+                      <img
+                        className="d-block"
+                        src={popularGames.image_url}
+                        onClick={() =>
+                          openInNewTab(
+                            `${process.env.PUBLIC_URL}/detail/${popularGames.id}`
+                          )
+                        }
+                        alt="carousel-images"
+                        style={{
+                          objectFit: 'contain',
+                          width: '50%',
+                          height: '40%',
+                        }}
+                      />
+                    </span>
+                  </div>
                 </div>
-            </Carousel.Item>
-            )
-          )};
+              </Carousel.Item>
+            ))}
+            ;
           </Carousel>
-          </div>
-        </Container>
-      </React.Fragment>
-);
-            }
+        </div>
+      </Container>
+    </React.Fragment>
+  );
+}
 export default Trending;
